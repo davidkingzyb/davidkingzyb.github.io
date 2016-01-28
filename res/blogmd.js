ce516b51796a94eb9c091430084135f821748d61
DKZ <davidkingzyb@163.com>
2016-01-28 15:12:24 +0800

    dkzlogo.js

---
+++ res/blogmd.js
@@ -0,0 +1,98 @@
+//////////////////////////////////////
+//  `_______  `__   __  `_______    //  
+//  |   __  \ |  | /  / |___   /    //  
+//  |  |  \  \|  |/  /     /  /     //  
+//  |  |  |  ||   _  \    /  /      //  
+//  |  |__|  ||  | \  \  /  /____   //  
+//  |________/|__|  \__\/________|  //  
+//////////////////////////////////////
+//  2016/01/28 by DKZ https://davidkingzyb.github.io
+
+var searchArr=[];
+
+function getSearchArr(){
+	var anchor=document.querySelectorAll(".anchor");
+	for(var i=0;i<anchor.length;i++){
+		searchArr.push(anchor[i].href.split('#')[1]);
+	}
+}
+
+function searchAnchor(key){
+	for(var i=0;i<searchArr.length;i++){
+		if(searchArr[i].indexOf(key)!==-1){
+			return i;
+		}
+	}
+	return -1;
+}
+
+function wrapCode(){
+	var wrap=document.getElementsByTagName('code');
+	for(var i=0;i<wrap.length;i++){
+		if(wrap[i].outerHTML){
+			wrap[i].outerHTML='<pre>'+wrap[i].outerHTML+'</pre>';
+		}else{
+			var tmp = document.createElement('pre');
+    		tmp.appendChild(wrap.cloneNode(true));
+    		wrap.parentNode.replaceChild(tmp, wrap);
+		}
+	}
+}
+
+window.onload=function(){
+	
+	var canvas=document.getElementById("dkzlogo");
+	var context=canvas.getContext('2d');
+	var DKZlogo=new DKZLogoClass(context,15);
+	DKZlogo.drawDKZ('stroke');
+	setTimeout(DKZlogo.animateDKZ(),2000);
+	canvas.onclick=function(){
+		DKZlogo.fillrandomDKZ();
+	};
+
+	getSearchArr();
+
+	var searchbtn=document.getElementById("search");
+
+	searchbtn.onclick=function(){
+		var key=encodeURI(prompt('Search:')).toLowerCase();
+		var index=searchAnchor(key);
+		window.scrollTo(0,document.getElementsByClassName('anchor')[index].getBoundingClientRect().top+document.body.scrollTop);
+	};
+
+	var footer=document.getElementById('footer');
+	function showFooter(h){
+		footer.style.height=h;
+	}
+	function hideFooter(h){
+		footer.style.height=h;
+	}
+	if(window.innerWidth>=750){
+		footer.onmouseover=function(){
+			showFooter('100px');
+		};
+		footer.onmouseout=function(){
+			hideFooter('20px');
+		};
+	}
+
+	window.onscroll=function(){
+		if(document.body.clientHeight-document.body.scrollTop-window.innerHeight<20){
+			if(window.innerWidth>=750){
+				footer.style.height='100px';
+			}
+		}else{
+			if(window.innerWidth>=750){
+				footer.style.height='20px';
+			}
+		}
+	};
+
+	var pathname=window.location.pathname.substring(1);
+	var threadkey=pathname.split('.')[0];
+	var url=window.location.href;
+	var dsstr='<div class="ds-thread" data-thread-key="'+threadkey+'" data-title="'+pathname+'" data-url="'+url+'"></div>';
+	document.getElementById('discusspane').innerHTML=dsstr;
+
+	wrapCode();
+};
